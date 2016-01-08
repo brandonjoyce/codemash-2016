b2fc24a8c76ec5d3e211e25d73f97fc454fc0f9a
Brandon Joyce <bjoyce@covermymeds.com>
2016-01-08 10:36:54 -0500

    slide additions, and code changes

--- 3.request_mapper.rb
+++ 3.request_mapper.rb
@@ -4,16 +4,20 @@ class CreateRequestMapper
     @document = document
     @patient_mapper = RequestMappers::PatientMapper.new document
     @prescriber_mapper = RequestMappers::PrescriberMapper.new document
-    # and so on...
+    @prescription_mapper = RequestMappers::PrescriptionMapper.new document
+    @payer_mapper = RequestMappers::PayerMapper.new document
   end
 
-  attr_reader :document, :patient_mapper, :prescriber_mapper
+  attr_reader :document, :patient_mapper, :prescriber_mapper,
+              :prescription_mapper, :payer_mapper
 
   def request_data
     {
       patient: patient_mapper.map,
       prescriber: prescriber_mapper.map,
-      # and so on...
+      prescription: prescription_mapper.map,
+      payer: payer_mapper.map,
+      state: document.patient.state,
     }
   end
 end
